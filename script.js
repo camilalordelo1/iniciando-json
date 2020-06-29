68069095309b71349a5ba3f2327cf254e49ec473
camilalordelo15@gmail.com <camilalordelo15@gmail.com>
2020-06-29 15:53:53 -0300

    base da tabela

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
-var tabela = {
-    "funcionarios":[
-        {"nome":"João da Silva", 
-        "skills":["Java","PHP","Javascript","React"]},
-        {"nome":"Renata Souza",
-        "skills":["Python","Angular"]},
-        {"nome":"Rodrigo Campos",
-        "skills":["Scrum","Project","Office","Gestão de Equipe"]},
-        {"nome":"Tarcisio Moura","skills":["ShellScript","Linux","Apache","Segurança"]},{"nome":"Joselito Marmita","skills":["Manutenção de PC","Cabeamento","Configuração"]},
-        {"nome":"Fernando Pessoa","skills":["Mídias Sociais","Marketing","Design"]}
-    ]
+function gerar(){
+    var tabela = {
+        "funcionarios":[
+            {
+                "nome":"João da Silva", 
+                "skills":["Java","PHP","Javascript","React"]
+            },
+            {
+                "nome":"Renata Souza",
+                "skills":["Python","Angular"]
+            },
+            {
+                "nome":"Rodrigo Campos",
+                "skills":["Scrum","Project","Office","Gestão de Equipe"]
+            },
+            {
+                "nome":"Tarcisio Moura",
+                "skills":["ShellScript","Linux","Apache","Segurança"]
+            },
+            {
+                "nome":"Joselito Marmita",
+                "skills":[
+                    "Manutenção de PC","Cabeamento","Configuração"]
+            },
+            {
+                "nome":"Fernando Pessoa",
+                "skills":["Mídias Sociais","Marketing","Design"]
+            }
+        ]
+    }
+    
+    res.innerHTML += `
+    <table border="1" class="border-1 mt-3" >
+        <tr>
+            <td class="pr-2 pl-2">
+                <p class="font-weight-bold">NOME</p>
+            </td>
+            <td class="pr-2 pl-2" >
+                <p class="font-weight-bold">SKILLS</p>
+            </td>
+        </tr>
+    </table>`
+    
 }
