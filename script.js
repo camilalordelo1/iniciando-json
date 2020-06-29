49c4e1a87e062443d7bb2fdcd6f773373098ab6f
camilalordelo15@gmail.com <camilalordelo15@gmail.com>
2020-06-29 13:51:05 -0300

    json de base

---
+++ script.js
@@ -0,0 +1,12 @@
+var tabela = {
+    "funcionarios":[
+        {"nome":"João da Silva", 
+        "skills":["Java","PHP","Javascript","React"]},
+        {"nome":"Renata Souza",
+        "skills":["Python","Angular"]},
+        {"nome":"Rodrigo Campos",
+        "skills":["Scrum","Project","Office","Gestão de Equipe"]},
+        {"nome":"Tarcisio Moura","skills":["ShellScript","Linux","Apache","Segurança"]},{"nome":"Joselito Marmita","skills":["Manutenção de PC","Cabeamento","Configuração"]},
+        {"nome":"Fernando Pessoa","skills":["Mídias Sociais","Marketing","Design"]}
+    ]
+}
